7566aec272ee821392918f90f2468f796ecfe608
IlyaGorelov <107054569+IlyaGorelov@users.noreply.github.com>
2026-07-19 17:27:56 +0300

    add creating of db table if it doesn't exist

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Abot2.Crawler;
 using Abot2.Poco;
+using Crawler.Context;
 using Crawler.Controllers;
 using Crawler.Entities;
 using Serilog;
@@ -15,6 +16,8 @@ namespace Crawler
                 .WriteTo.Console()
                 .MinimumLevel.Information()
                 .CreateLogger();
+
+            new FindomContext().Database.EnsureCreated();
 
             const string URL = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0";
             Option<string> urlToCrawlOption = new("--url", "-u")
